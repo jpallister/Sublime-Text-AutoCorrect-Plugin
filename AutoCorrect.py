a4c7a642fba7ec84ac4665511c378f1c8cbbe328
James Pallister <jp8257@bristol.ac.uk>
2012-03-29 16:12:17 +0100

    Multiple curses undo fix

--- AutoCorrect.py
+++ AutoCorrect.py
@@ -2,6 +2,7 @@
 import sublime_plugin
 import SubList
 import sublime
+import functools
 
 # Eventually this should be end user updatable
 corrections = {"bitwdith": "bit-width", "wdith": "width"}
@@ -15,33 +16,36 @@ class AutoCorrectCommand(sublime_plugin.TextCommand):
 
         regions_to_correct = []
 
+        self.view.run_command("insert", {"characters": " "})
+
         for r in rs:
             if r.b - r.a == 0:
-                word = self.view.word(r)
+                word = self.view.word(r.begin() - 1)
                 sword = self.view.substr(word)
 
                 can_local_correct = sword in corrections  # locally set should take priority
                 correct = SubList.match(sword)
 
-                if max(word.a, word.b) <= r.a and (can_local_correct or correct != ""):
+                if word.end() <= r.begin() - 1 and (can_local_correct or correct != ""):
                     if can_local_correct:
                         correct = corrections[sword]
 
                     regions_to_correct.append((correct, word))
                     sublime.status_message("AutoCorrect: \"" + sword + "\" corrected to \"" + correct + "\"")
 
-        self.view.run_command("insert", {"characters": " "})
-
         self.view.end_edit(edit)
         edit = self.view.begin_edit()
 
-        for (correct, word) in regions_to_correct:
+        for i, (correct, word) in enumerate(regions_to_correct[::-1]):
+            reg = self.view.word(word)
+            self.view.replace(edit, reg, correct)
 
-            self.view.replace(edit, word, correct)
+            self.view.add_regions("correction_outline" + str(i), [self.view.word(reg.begin())], "mark", sublime.DRAW_EMPTY)
 
-            self.view.add_regions("correction_outline", [self.view.word(word)], "mark", sublime.DRAW_EMPTY)
-            sublime.set_timeout(lambda: self.view.erase_regions("correction_outline"), 500)
+        sublime.set_timeout(functools.partial(self.remove_regions, len(regions_to_correct)), 500)
 
         self.view.end_edit(edit)
 
-
+    def remove_regions(self, n):
+        for i in range(n):
+            self.view.erase_regions("correction_outline" + str(i))
